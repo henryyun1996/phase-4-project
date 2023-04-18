3f5590b427994fc698c3e70a1f17575134798db6
Henry Yun <henrysungyun@gmail.com>
2023-04-18 09:37:40 -0700

    got some frontend pages to render

---
+++ client/src/components/LogIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function LogIn() {
+    return (
+        <div>
+            <h1>Log In Page</h1>
+        </div>
+    )
+}
+
+export default LogIn;
